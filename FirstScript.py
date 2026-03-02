34a8300d63cc6bf87a5b5098ed378e5e5bf731be
CarlosMZ-179760 <0610806283@uma.es>
2026-03-02 20:01:14 +0100

    Primer script
    De momento, se crea un script de prueba para el detector ensemble y sus argumentos por defecto (CUSUM, ADWIN, y HDDM-A)

---
+++ FirstScript.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Un primer script de prueba breve para el detector ensemble 
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import capymoa.drift.detectors.ensemble_detector as ensemble
+
+data_stream_generator = np.random.default_rng(seed=179760)
+data_stream=np.absolute(data_stream_generator.normal(loc=0, scale=1, size=2000))
+for i in range(999, 2000):
+    data_stream[i] = np.absolute(data_stream_generator.normal(loc=0, scale=2))
+data_stream_average=[]
+rang = 10
+x=np.arange(rang,2000)
+for ind in range(rang):
+    data_stream_average.append(np.mean(data_stream[:ind]))
+for ind in range(len(data_stream)-rang +1):
+    data_stream_average.append(np.mean(data_stream[ind:ind+rang]))
+#plt.plot(x,data_stream_average[rang+1:])
+
+
+detector = ensemble.EnsembleDetector()
+for i in range(2000):
+    detector.add_element(data_stream[i])
+    #print(detector.get_states())
+print(detector.get_all_base_detections())
+#print(detector.get_all_base_warnings())
